5fade6a9bf06c43d2ca6865d416761a4d7650a01
colin11702 <colinmcherney@gmail.com>
2023-07-03 20:37:53 -0700

    Added playRound function to js
    Added function to take care of the logic of the game.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function getComputerChoice() {
+function computerSelection() {
   let randnum = Math.floor(Math.random() * 3);
   let choices = ["rock", "paper", "scissors"];
-  let decision = choices[randnum];
-  return decision;
+  let computerDecision = choices[randnum];
+  return computerDecision;
 }
 
 function playerSelection() {
@@ -14,4 +14,28 @@ function playerSelection() {
   }
 }
 
-console.log(playerSelection());
+function playRound(computer, player) {
+  if (computer == player) {
+    console.log("Tie Game!");
+  }
+  if (computer == "rock" && player == "paper") {
+    console.log("The computer chose " + computer + "! You Won!");
+  }
+  if (computer == "rock" && player == "scissors") {
+    console.log("The computer chose " + computer + "! You Lost!");
+  }
+  if (computer == "paper" && player == "rock") {
+    console.log("The computer chose " + computer + "! You Lost!");
+  }
+  if (computer == "paper" && player == "scissors") {
+    console.log("The computer chose " + computer + "! You Won!");
+  }
+  if (computer == "scissors" && player == "rock") {
+    console.log("The computer chose " + computer + "! You Won!");
+  }
+  if (computer == "scissors" && player == "paper") {
+    console.log("The computer chose " + computer + "! You Lost!");
+  }
+}
+
+playRound(computerSelection(), playerSelection());
