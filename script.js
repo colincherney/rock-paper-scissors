d7710b176f3280345da7d052708ea4d4ec021b33
colin11702 <colinmcherney@gmail.com>
2023-07-10 21:02:23 -0700

    Added Scoreboard and Reset Button
    Added updating scoreboard on the webpage and a reset button

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-function playRound(player) {
-  let randnum = Math.floor(Math.random() * 3);
-  let choices = ["rock", "paper", "scissors"];
-  let computer = choices[randnum];
+let personWinCounter = 0;
+let computerWinCounter = 0;
 
-  if (computer == player) {
-    console.log("Tie Game!");
+function playGame(player) {
+  // first player to 5 wins game
+
+  if (personWinCounter < 5 && computerWinCounter < 5) {
+    let randnum = Math.floor(Math.random() * 3);
+    let choices = ["rock", "paper", "scissors"];
+    let computer = choices[randnum];
+
+    if (computer == player) {
+      document.getElementById("print").innerHTML = "Tie Game!";
+    }
+    if (computer == "rock" && player == "paper") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Won!";
+      personWinCounter++;
+      document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+    }
+    if (computer == "rock" && player == "scissors") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Lost!";
+      computerWinCounter++;
+      document.getElementById("computerScoreUpdate").innerHTML =
+        computerWinCounter;
+    }
+    if (computer == "paper" && player == "rock") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Lost!";
+      computerWinCounter++;
+      document.getElementById("computerScoreUpdate").innerHTML =
+        computerWinCounter;
+    }
+    if (computer == "paper" && player == "scissors") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Won!";
+      personWinCounter++;
+      document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+    }
+    if (computer == "scissors" && player == "rock") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Won!";
+      personWinCounter++;
+      document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+    }
+    if (computer == "scissors" && player == "paper") {
+      document.getElementById("print").innerHTML =
+        "The computer chose " + computer + "! You Lost!";
+      computerWinCounter++;
+      document.getElementById("computerScoreUpdate").innerHTML =
+        computerWinCounter;
+    }
   }
-  if (computer == "rock" && player == "paper") {
-    console.log("The computer chose " + computer + "! You Won!");
+  // if player wins
+  if (personWinCounter == 5) {
+    document.getElementById("outcome").innerHTML = "You Win!";
   }
-  if (computer == "rock" && player == "scissors") {
-    console.log("The computer chose " + computer + "! You Lost!");
-  }
-  if (computer == "paper" && player == "rock") {
-    console.log("The computer chose " + computer + "! You Lost!");
-  }
-  if (computer == "paper" && player == "scissors") {
-    console.log("The computer chose " + computer + "! You Won!");
-  }
-  if (computer == "scissors" && player == "rock") {
-    console.log("The computer chose " + computer + "! You Won!");
-  }
-  if (computer == "scissors" && player == "paper") {
-    console.log("The computer chose " + computer + "! You Lost!");
+  // if computer wins
+  if (computerWinCounter == 5) {
+    document.getElementById("outcome").innerHTML = "You Lost";
   }
 }
 
-// function game() {
-//   let x = 0;
-//   while (x <= 4) {
-//     playRound(computerSelection(), playerSelection());
-//     x++;
-//   }
-// }
+function resetGame() {
+  personWinCounter = 0;
+  computerWinCounter = 0;
 
-// game();
+  document.getElementById("outcome").innerHTML = "";
+  document.getElementById("print").innerHTML = "";
+  document.getElementById("computerScoreUpdate").innerHTML = 0;
+  document.getElementById("playerScoreUpdate").innerHTML = 0;
+}
