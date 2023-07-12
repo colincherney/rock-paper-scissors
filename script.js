45a4f448255f88cfa3fbee06eedaf32895c1cbe2
colin11702 <colinmcherney@gmail.com>
2023-07-11 18:46:15 -0700

    Added new way to see outcome
    made it so image changes weather you win or lose and then it goes back to original.

--- script.js
+++ script.js
@@ -9,47 +9,53 @@ function playGame(player) {
     let choices = ["rock", "paper", "scissors"];
     let computer = choices[randnum];
 
+    let og = "";
+
+    if (player == "rock") {
+      og = "images/rock.png";
+    }
+    if (player == "paper") {
+      og = "images/paper.png";
+    }
+    if (player == "scissors") {
+      og = "images/scissors.png";
+    }
+
     if (computer == player) {
-      document.getElementById("print").innerHTML = "Tie Game!";
+      imageChange(player, "images/draw.png", og);
     }
     if (computer == "rock" && player == "paper") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Won!";
       personWinCounter++;
       document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+      imageChange(player, "images/check.png", og);
     }
     if (computer == "rock" && player == "scissors") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Lost!";
       computerWinCounter++;
       document.getElementById("computerScoreUpdate").innerHTML =
         computerWinCounter;
+      imageChange(player, "images/red-x.png", og);
     }
     if (computer == "paper" && player == "rock") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Lost!";
       computerWinCounter++;
       document.getElementById("computerScoreUpdate").innerHTML =
         computerWinCounter;
+      imageChange(player, "images/red-x.png", og);
     }
     if (computer == "paper" && player == "scissors") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Won!";
       personWinCounter++;
       document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+      imageChange(player, "images/check.png", og);
     }
     if (computer == "scissors" && player == "rock") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Won!";
       personWinCounter++;
       document.getElementById("playerScoreUpdate").innerHTML = personWinCounter;
+      imageChange(player, "images/check.png", og);
     }
     if (computer == "scissors" && player == "paper") {
-      document.getElementById("print").innerHTML =
-        "The computer chose " + computer + "! You Lost!";
       computerWinCounter++;
       document.getElementById("computerScoreUpdate").innerHTML =
         computerWinCounter;
+      imageChange(player, "images/red-x.png", og);
     }
   }
   // if player wins
@@ -66,8 +72,13 @@ function resetGame() {
   personWinCounter = 0;
   computerWinCounter = 0;
 
-  document.getElementById("outcome").innerHTML = "";
-  document.getElementById("print").innerHTML = "";
   document.getElementById("computerScoreUpdate").innerHTML = 0;
   document.getElementById("playerScoreUpdate").innerHTML = 0;
+}
+
+function imageChange(id, result, original) {
+  document.getElementById(id).setAttribute("src", result);
+  setTimeout(() => {
+    document.getElementById(id).setAttribute("src", original);
+  }, 500);
 }
